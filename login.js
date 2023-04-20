59954017f60843c99eba29c8f86b8a155be49609
NemesisPrasa <129283828+NemesisPrasa@users.noreply.github.com>
2023-04-20 13:27:43 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    // Load the user data from the JSON file using Ajax
+    $.getJSON('registration.json', function(data) {
+      var users = data;
+  
+      // Add click event listener to the submit button
+      $('#RegSubmit').click(function(e) {
+        e.preventDefault();
+  
+        // Get form values
+        var username = $('#userNameReg').val();
+        var email = $('#emailReg').val();
+        var password = $('#passwordReg').val();
+
+        console.log(`User ${username} registered with email ${email} and password ${password}`);
+  
+        // Check if user already exists
+        if (users.hasOwnProperty(username)) {
+          alert('User already exists, please login.');
+          return;
+        }
+  
+        // Add new user to the users object
+        users[username] = {
+          email: email,
+          password: password
+        };
+  
+        // Save user data to the JSON file using Ajax
+        $.ajax({
+          type: 'POST',
+          url: 'registration.json',
+          data: JSON.stringify(users),
+          contentType: 'application/json',
+          success: function() {
+            // Redirect to the main site
+            window.location.href = 'index.html';
+            console.log(users);
+          },
+          error: function() {
+            alert('Error saving user data.');
+          }
+        });
+      });
+  
+      // Add click event listener to the login button
+      $('#LoginButton').click(function(e) {
+        e.preventDefault();
+  
+        // Get form values
+        var username = $('#userEmailLogin').val();
+        var password = $('#userPasswordLogin').val();
+  
+        // Check if user exists and password is correct
+        if (!users.hasOwnProperty(username) || users[username].password !== password) {
+          alert('Incorrect username or password, please try again.');
+          return;
+        }
+  
+        // Redirect to the main site
+        window.location.href = 'index.html';
+      });
+    });
+  });
+  
+
--- script.js
+++ script.js
@@ -31,8 +31,45 @@ function sendEmail(){
 
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar');
+const navBarClose = document.querySelector('.closeNavbar');
+const navClose = document.querySelector('.navClose');
 
 navbarToggle.addEventListener('click', () => {
   navbarToggle.classList.toggle('active');
   navbarMenu.classList.toggle('active');
 });
+
+
+const warepper = document.querySelector('.warepper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const buttonPopUp = document.querySelector('.logginPopup');
+const closePopUp = document.querySelector('.icon-close');
+var focuss = document.querySelector('#all');
+const buttonToggle = document.querySelector('#logginPopup2');
+
+
+registerLink.addEventListener('click', () => {
+  warepper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+  warepper.classList.remove('active');
+});
+
+buttonPopUp.addEventListener('click', () => {
+  warepper.classList.add('active-popUp');
+  focuss.style.opacity = 0.2;
+});
+
+closePopUp.addEventListener('click', () => {
+  warepper.classList.remove('active-popUp');
+  focuss.style.opacity = 1;
+});
+
+buttonToggle.addEventListener('click', () => {
+  warepper.classList.add('active-popUp');
+  focuss.style.opacity = 0.2;
+});
+
+
